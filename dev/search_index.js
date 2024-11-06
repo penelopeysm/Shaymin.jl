9767def794c3fae67c2e25ba3228cd22ecb2b042
Documenter.jl <documenter@juliadocs.github.io>
2024-11-06 00:21:33 +0000

    build based on ed31e9b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Shaymin.jl","page":"Shaymin.jl","title":"Shaymin.jl","text":"","category":"section"},{"location":"","page":"Shaymin.jl","title":"Shaymin.jl","text":"(Image: Shaymin)","category":"page"}]
+}
