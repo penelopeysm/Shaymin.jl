c763a82dba1fccb6717eab04195e2ef6f8660c3a
Penelope Yong <penelopeysm@gmail.com>
2024-12-10 15:28:05 +0000

    Add a doctest

--- src/Shaymin.jl
+++ src/Shaymin.jl
@@ -7,7 +7,17 @@ using Distributions
 """
     shay()
 
-    Generate a very cute number from a normal distribution.
+Generate a very cute number from a normal distribution.
+
+```jldoctest
+julia> using Shaymin, Random
+
+julia> Random.seed!(492)
+TaskLocalRNG()
+
+julia> shay()
+-0.7193893056115281
+```
 """
 function shay()
     return rand(Normal(0, 1))
---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using Documenter
+using Random
+using Shaymin
+using Test
+
+@testset "doctests" begin
+    doctest(Shaymin)
+end
