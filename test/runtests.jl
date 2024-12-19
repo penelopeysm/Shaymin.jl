3a74d08774ada10c403f5a64005492c16938f3b9
Penelope Yong <penelopeysm@gmail.com>
2024-12-19 12:46:20 +0000

    csmc only

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,29 +6,26 @@ using Turing
 Random.seed!(468)
 
 @testset verbose = true "Shaymin.jl" begin
-    @testset "randn" begin
-        x1 = Random.randn(10)
+    @testset "using global seed" begin
+        x1 = randn(3)
         @info x1
-        @test length(x1) == 10
     end
 
     @testset "stablerng" begin
-        x2 = rand(StableRNG(468), 10)
+        x2 = randn(StableRNG(468), 3)
         @info x2
-        @test length(x2) == 10
     end
 
     @testset "ess"  begin
         @model function f(y)
             a ~ Normal(0, 1)
-            b ~ Normal(a, 1)
-            y ~ Normal(b, 1)
+            y ~ Normal(a, 1)
         end
 
         Random.seed!(468)
-        alg = Gibbs(ESS(:a), ESS(:b))
+        alg = Gibbs(CSMC(15, :a))
         chain = sample(StableRNG(468), f(1.5), alg, 50; progress=false)
         @show mean(chain[:a])
-        @show mean(chain[:b])
+        # @show mean(chain[:b])
     end
 end
