d908072d321c44442cf9faa24d069821cfbdba47
Penelope Yong <penelopeysm@gmail.com>
2024-12-19 02:51:07 +0000

    Randn

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,19 @@
 using Test
 using StableRNGs
+using Random
+
+Random.seed!(468)
 
 @testset verbose = true "Shaymin.jl" begin
-    x = rand(StableRNG(1), 10)
-    @info x
-    @test length(x) == 10
+    @testset "randn" begin
+        x1 = Random.randn(10)
+        @info x1
+        @test length(x1) == 10
+    end
+
+    @testset "stablerng" begin
+        x2 = rand(StableRNG(1), 10)
+        @info x2
+        @test length(x2) == 10
+    end
 end
