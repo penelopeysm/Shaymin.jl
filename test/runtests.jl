84d515784a613fed3f57245b087429fada4a17e8
Penelope Yong <penelopeysm@gmail.com>
2024-12-19 13:05:22 +0000

    trim

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,7 @@
 using Test
 using StableRNGs
 using Random
-using DynamicPPL
-using Distributions
 using AdvancedPS
-using Random123
 
 Random.seed!(468)
 
@@ -19,13 +16,8 @@ Random.seed!(468)
         @info x2
     end
 
-    @testset "random123" begin
-        x3 = randn(Random123.Philox2x(1), 3)
-        @info x3
-    end
-
     @testset "advancedps" begin
-        x4 = randn(AdvancedPS.TracedRNG(), 3)
-        @info x4
+        x3 = randn(AdvancedPS.TracedRNG(), 3)
+        @info x3
     end
 end
