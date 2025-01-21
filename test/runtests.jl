620722224ce62810b399218d22e8bfdebfcbd9aa
Penelope Yong <penelopeysm@gmail.com>
2025-01-21 15:25:19 +0000

    this

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,162 +10,6 @@ using Pkg
 using AdvancedPS
 using Random123
 
-function AdvancedPS.gen_seed(rng::Random.AbstractRNG, ::AdvancedPS.TracedRNG{<:Integer}, sampler::Random.Sampler)
-    x = Random.rand(rng, sampler)
-    @show "1", rng, x
-    return x
-end
-function AdvancedPS.seed_from_rng!(
-    pc::AdvancedPS.ParticleContainer{T,<:AdvancedPS.TracedRNG{R,N,<:Random123.AbstractR123{I}}},
-    rng::Random.AbstractRNG,
-    ref::Union{AdvancedPS.Particle,Nothing}=nothing,
-) where {T,R,N,I}
-    n = length(pc.vals)
-    nseeds = isnothing(ref) ? n : n - 1
-    @show I
-
-    sampler = Random.Sampler(rng, I)
-    @show sampler
-    for i in 1:nseeds
-        subrng = pc.vals[i].rng
-        Random.seed!(subrng, AdvancedPS.gen_seed(rng, subrng, sampler))
-    end
-    Random.seed!(pc.rng, AdvancedPS.gen_seed(rng, pc.rng, sampler))
-    println("1")
-    @show [v.rng for v in pc.vals]
-    return pc
-end
-
-function DynamicPPL.initialstep(
-    rng::Random.AbstractRNG,
-    model::AbstractMCMC.AbstractModel,
-    spl::DynamicPPL.Sampler{<:PG},
-    vi::DynamicPPL.AbstractVarInfo;
-    kwargs...,
-)
-    # Reset the VarInfo before new sweep
-    DynamicPPL.reset_num_produce!(vi)
-    DynamicPPL.set_retained_vns_del_by_spl!(vi, spl)
-    DynamicPPL.resetlogp!!(vi)
-
-    # Create a new set of particles
-    num_particles = spl.alg.nparticles
-    particles = AdvancedPS.ParticleContainer(
-        [AdvancedPS.Trace(model, spl, vi, AdvancedPS.TracedRNG()) for _ in 1:num_particles],
-        AdvancedPS.TracedRNG(),
-        rng,
-    )
-    println("2")
-    @show [v.rng for v in particles.vals]
-
-    # Perform a particle sweep.
-    logevidence = AdvancedPS.sweep!(rng, particles, spl.alg.resampler, spl)
-    @show logevidence
-    println("3")
-    @show [v.rng for v in particles.vals]
-
-    # Pick a particle to be retained.
-    Ws = AdvancedPS.getweights(particles)
-    indx = AdvancedPS.randcat(rng, Ws)
-    @show indx
-    reference = particles.vals[indx]
-
-    # Compute the first transition.
-    _vi = reference.model.f.varinfo
-    transition = Turing.Inference.PGTransition(model, _vi, logevidence)
-    state = Turing.Inference.PGState(_vi, reference.rng)
-
-    @show transition
-    @show state
-
-    return transition, state
-end
-
-function AdvancedPS.resample_propagate!(
-    ::Random.AbstractRNG,
-    pc::AdvancedPS.ParticleContainer,
-    sampler::T,
-    randcat=AdvancedPS.DEFAULT_RESAMPLER,
-    ref::Union{AdvancedPS.Particle,Nothing}=nothing;
-    weights=AdvancedPS.getweights(pc),
-) where {T<:AbstractMCMC.AbstractSampler}
-    # sample ancestor indices
-    n = length(pc)
-    nresamples = ref === nothing ? n : n - 1
-    indx = randcat(pc.rng, weights, nresamples)
-    println("resample_propagate!")
-    @show indx
-
-    # count number of children for each particle
-    num_children = zeros(Int, n)
-    @inbounds for i in indx
-        num_children[i] += 1
-    end
-
-    # fork particles
-    particles = collect(pc)
-    children = similar(particles)
-    j = 0
-    @inbounds for i in 1:n
-        ni = num_children[i]
-        if ni > 0
-            # fork first child
-            pi = particles[i]
-            isref = pi === ref
-            p = isref ? AdvancedPS.fork(pi, isref) : pi
-
-            key = isref ? AdvancedPS.safe_get_refseed(ref.rng) : AdvancedPS.state(p.rng.rng) # Pick up the alternative rng stream if using the reference particle
-            nsplits = isref ? ni + 1 : ni # We need one more seed to refresh the alternative rng stream
-            seeds = split(key, nsplits)
-            isref && AdvancedPS.safe_set_refseed!(ref.rng, seeds[end]) # Refresh the alternative rng stream
-
-            Random.seed!(p.rng, seeds[1])
-
-            children[j += 1] = p
-            # fork additional children
-            for k in 2:ni
-                part = AdvancedPS.fork(p, isref)
-                Random.seed!(part.rng, seeds[k])
-                children[j += 1] = part
-            end
-        end
-    end
-
-    if ref !== nothing
-        # Insert the retained particle. This is based on the replaying trick for efficiency
-        # reasons. If we implement PG using task copying, we need to store Nx * T particles!
-        AdvancedPS.update_ref!(ref, pc, sampler)
-        @inbounds children[n] = ref
-    end
-
-    # replace particles and log weights in the container with new particles and weights
-    pc.vals = children
-    AdvancedPS.reset_logweights!(pc)
-
-    return pc
-end
-
-function AdvancedPS.resample_propagate!(
-    rng::Random.AbstractRNG,
-    pc::AdvancedPS.ParticleContainer,
-    sampler::T,
-    resampler::AdvancedPS.ResampleWithESSThreshold,
-    ref::Union{AdvancedPS.Particle,Nothing}=nothing;
-    weights=AdvancedPS.getweights(pc),
-) where {T<:AbstractMCMC.AbstractSampler}
-    # Compute the effective sample size ``1 / ∑ wᵢ²`` with normalized weights ``wᵢ``
-    ess = inv(sum(abs2, weights))
-
-    if ess ≤ resampler.threshold * length(pc)
-        AdvancedPS.resample_propagate!(rng, pc, sampler, resampler.resampler, ref; weights=weights)
-    else
-        println("update_keys!")
-        AdvancedPS.update_keys!(pc, ref)
-    end
-
-    return pc
-end
-
 function AdvancedPS.update_keys!(pc::AdvancedPS.ParticleContainer, ref::Union{AdvancedPS.Particle,Nothing}=nothing)
     # Update keys to new particle ids
     println("update_keys! begin")
@@ -275,14 +119,14 @@ Distributions.logpdf(::MyNorm, x) = logpdf(Normal(), x)
         @show hash(UInt64(468), UInt(3))
     end
 
-    # reproducibly different
-    # @testset "pg"  begin
-    #     @model function f()
-    #         x ~ MyNorm()
-    #     end
-    #     Random.seed!(468)
-    #     alg = PG(15)
-    #     chain = sample(StableRNG(468), f(), alg, 10; progress=false)
-    #     @show mean(chain[:x])
-    # end
+    reproducibly different
+    @testset "pg"  begin
+        @model function f()
+            x ~ MyNorm()
+        end
+        Random.seed!(468)
+        alg = PG(15)
+        chain = sample(StableRNG(468), f(), alg, 10; progress=false)
+        @show mean(chain[:x])
+    end
 end
