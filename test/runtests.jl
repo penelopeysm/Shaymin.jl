6b360531959f0029fa65b2566e216241913ae5db
Penelope Yong <penelopeysm@gmail.com>
2025-04-10 16:08:32 +0100

    fmt

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Statistics: mean, var
 using StableRNGs
 
 # patch as per PR
-function AdvancedPS.split(key::Integer, n::Integer=1)
+function AdvancedPS.split(key::Integer, n::Integer = 1)
     T = typeof(key)
     inner_rng = Random.MersenneTwister(key)
     return rand(inner_rng, T, n)
@@ -15,7 +15,7 @@ end
 @testset verbose = true "Shaymin.jl" begin
     @testset "pg reproducibility" begin
         @model f() = x ~ Normal()
-        chain = sample(StableRNG(468), f(), PG(15), 500; progress=false)
+        chain = sample(StableRNG(468), f(), PG(15), 500; progress = false)
 
         @show mean(chain[:x])
         @show var(chain[:x])
