d40a818b8b4c5a74cc2c6c87c621e3820329790d
Penelope Yong <penelopeysm@gmail.com>
2026-02-28 03:04:51 +0000

    remove

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,17 @@
 using Enzyme: Forward, Reverse, jacobian
 
-struct Foo1{Trng}
-    ranges::Trng
+struct F{S}
+    ranges::S
 end
-struct Foo2 end
 
-function (t::Foo1)(x::AbstractArray{T}) where {T}
+function (t::F)(x::AbstractArray{T}) where {T}
     total_length = sum(length, t.ranges)
     y = Vector{T}(undef, total_length)
     y[t.ranges[1]] = [x[1]]
     return y
 end
-# function (t::Foo2)(y::AbstractVector{T}) where {T}
-#     return y
-#     # x = Vector{T}(undef, 1)
-#     # x[1] = view(y, t.ranges[1])[]
-#     # return x
-# end
 
-f = Foo1((1:1,))
+# must be composed with identity for it to fail
+f = F((1:1,)) ∘ identity
 x = randn(1)
 jacobian(Forward, f, x)
