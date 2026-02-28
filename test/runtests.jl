ae030cb7aee43b7c40ad470db33377c3342cabe8
Penelope Yong <penelopeysm@gmail.com>
2026-02-28 02:58:29 +0000

    one range only no tuple

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,29 @@
 using Enzyme: Forward, Reverse, jacobian
 
 struct ProductVecTransform{Trng}
-    ranges::Trng
+    range::Trng
 end
 
 struct ProductVecInvTransform{Trng}
-    ranges::Trng
+    range::Trng
 end
 
 function (t::ProductVecTransform)(x::AbstractArray{T}) where {T}
-    total_length = sum(length, t.ranges)
+    total_length = length(t.range)
     y = Vector{T}(undef, total_length)
-    y[t.ranges[1]] = [x[1]]
-#     return y
-# end
-# function (t::ProductVecInvTransform)(y::AbstractVector{T}) where {T}
+    y[t.range] = [x[1]]
+    return y
+end
+function (t::ProductVecInvTransform)(y::AbstractVector{T}) where {T}
     x = Vector{T}(undef, 1)
-    x[1] = view(y, t.ranges[1])[]
+    x[1] = view(y, t.range)[]
     return x
 end
 
-ffwd = ProductVecTransform((1:1,))
-frvs = ProductVecInvTransform((1:1,))
+ffwd = ProductVecTransform(1:1)
+frvs = ProductVecInvTransform(1:1)
 
-f = ffwd
+f = ffwd ∘ frvs
 xvec = randn(1)
 jacobian(Forward, f, xvec)
 
