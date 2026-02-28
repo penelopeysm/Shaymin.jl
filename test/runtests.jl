7370b5c1b78ea98dbfc3e2b10d647cc2e0ebfda1
Penelope Yong <penelopeysm@gmail.com>
2026-02-28 02:48:34 +0000

    inline inner fns

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,22 +5,32 @@ import Enzyme as E
 vec_wrap(x) = [x]
 only_wrap(x) = x[]
 
-struct ProductVecTransform{T1,T2,Trng}
-    fwd_transforms::T1
-    rvs_transforms::T2
+struct ProductVecTransform{TTrf,Trng}
+    transforms::TTrf
+    ranges::Trng
+end
+
+struct ProductVecInvTransform{TTrf,Trng}
+    transforms::TTrf
     ranges::Trng
 end
 
 function (t::ProductVecTransform)(x::AbstractArray{T}) where {T}
     total_length = sum(length, t.ranges)
     y = Vector{T}(undef, total_length)
-    y[t.ranges[1]] = t.fwd_transforms[1](x[1])
+    y[t.ranges[1]] = [x[1]]
+    return y
+end
+function (t::ProductVecInvTransform)(y::AbstractVector{T}) where {T}
     x = Vector{T}(undef, 1)
-    x[1] = t.rvs_transforms[1](view(y, t.ranges[1]))
+    x[1] = view(y, t.ranges[1])[]
     return x
 end
 
-f = ProductVecTransform((vec_wrap,), (only_wrap,), (1:1,))
+ffwd = ProductVecTransform((vec_wrap,), (1:1,))
+frvs = ProductVecInvTransform((only_wrap,), (1:1,))
+
+f = ffwd ∘ frvs
 xvec = randn(1)
 E.jacobian(E.Forward, f, xvec)
 
