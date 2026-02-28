cd2f8d178ae3818a0ec193507c24f40638a17900
Penelope Yong <penelopeysm@gmail.com>
2026-02-28 02:27:04 +0000

    rg

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,30 +6,32 @@ import Enzyme as E
 vec_wrap(x) = [x]
 only_wrap(x) = x[]
 
-struct Fwd{F,D}
+struct Fwd{F,Trng}
     f::F
-    base_size::D
+    ranges::Trng
 end
 function (t::Fwd)(x::AbstractVector{T}) where {T}
     y = Vector{T}(undef, 1)
-    y[1:1] = t.f(x[1])
+    y[t.ranges[1]] = t.f(x[1])
     return y
 end
 
-struct Rvs{F,D}
+struct Rvs{F,Trng}
     f::F
-    base_size::D
+    ranges::Trng
 end
 function (t::Rvs)(y::AbstractVector{T}) where {T}
     x = Vector{T}(undef, 1)
-    x[1] = t.f(view(y, 1:1))
+    x[1] = t.f(view(y, t.ranges[1]))
     return x
 end
 
-ffwd = Fwd(vec_wrap, ())
-frvs = Rvs(only_wrap, ())
+ffwd = Fwd(vec_wrap, (1:1,))
+frvs = Rvs(only_wrap, (1:1,))
 
 adtype = DI.AutoEnzyme(; mode=E.Forward, function_annotation=E.Const)
 
 xvec = randn(1)
 DI.jacobian(ffwd ∘ frvs, adtype, xvec)
+
+@info "Done"
