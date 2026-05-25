4b09c2d0d8af0429d5a0893b458747c6733cdde3
Penelope Yong <penelopeysm@gmail.com>
2026-05-25 01:15:40 +0100

    codeunits

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,5 @@
-using InteractiveUtils: versioninfo
-versioninfo()
-
-using Enzyme: Forward, Reverse, jacobian, Enzyme
-Enzyme.Compiler.VERBOSE_ERRORS[] = true
-
-struct F{S}
-    ranges::S
-end
-
-function (t::F)(x::AbstractArray{T}) where {T}
-    total_length = sum(length, t.ranges)
-    y = Vector{T}(undef, total_length)
-    y[1:1] = [x[1]]
-    return y
-end
-
-# must be composed with identity for it to fail
-f = F((1:1,)) ∘ identity
-x = randn(1)
-jacobian(Forward, f, x)
+s = raw"""
+hello
+world
+"""
+@show collect(codeunits(s))
