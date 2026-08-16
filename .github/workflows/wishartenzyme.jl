718df67f7fed26718a5d674417ec562d5810d039
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 22:11:13 +0100

    do we even need that?

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -2,9 +2,6 @@ using Enzyme
 
 function invpd(yvec::AbstractVector{T}) where {T<:Real}
     X = zeros(T, 2, 2)
-    X[1, 1] = yvec[1]
-    X[1, 2] = yvec[2]
-    X[2, 1] = yvec[3]
     return X * X'
 end
 xv = randn(3)
