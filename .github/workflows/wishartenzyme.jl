04140da868bb83ee012875567646966e4b2b69e1
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 22:36:08 +0100

    maybe just 1

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -2,9 +2,10 @@ using Enzyme
 
 function invpd(yvec::AbstractVector{T}) where {T<:Real}
     X = zeros(T, 2, 2)
+    X[1, 1] = yvec[1]
     return X * X'
 end
-xv = randn(3)
+xv = randn(1)
 f(x) = sum(invpd(x))
 
 @info f(xv)
