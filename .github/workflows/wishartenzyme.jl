4d25b4a7f60a52a613db32ff2140c8bda06c7d3a
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 21:51:43 +0100

    put 2 back

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -1,9 +1,9 @@
 using Enzyme
 
-function invpd(yvec::AbstractVector{T}) where {T<:Real}
-    X = zeros(T, 2, 2)
+function invpd(d::Int, yvec::AbstractVector{T}) where {T<:Real}
+    X = zeros(T, d, d)
     idx = 1
-    for i in 1:2
+    for i in 1:d
         for j in 1:i
             X[i, j] = yvec[idx]
             idx += 1
@@ -12,7 +12,7 @@ function invpd(yvec::AbstractVector{T}) where {T<:Real}
     return X
 end
 xv = randn(3)
-f(x) = sum(invpd(x))
+f(x) = sum(invpd(2, x))
 
 @info f(xv)
 @info gradient(Reverse, Const(f), xv)
