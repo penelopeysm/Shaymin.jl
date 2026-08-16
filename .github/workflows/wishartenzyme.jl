ea57817c92bdfe0ef8d9e0f61463f04d8d02948c
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 22:43:09 +0100

    how about this?

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -1,11 +1,10 @@
 using Enzyme
 
 function f(y)
-    X = zeros(2, 2)
+    X = zeros(1, 1)
     X[1, 1] = y[1]
     return sum(X * X')
 end
-yv = randn(1)
-@info f(yv)
-
-@info gradient(Reverse, Const(f), yv)
+y = [0.5]
+@info f(y)
+@info gradient(Reverse, Const(f), y)
