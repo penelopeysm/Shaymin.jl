247b280a61892abcb0e8bdaf95fc4db15dadc97b
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 20:57:13 +0100

    OK maybe needs Bijectors

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -3,11 +3,13 @@
 # adtype = AutoEnzyme(; mode=set_runtime_activity(Reverse), function_annotation=Const)
 # DynamicPPL.TestUtils.AD.run_ad(f(), adtype; test=false, benchmark=true)
 
-using Distributions, Enzyme
+using Distributions, Enzyme, Bijectors
 
 const d = Wishart(7, [1.0 0.5; 0.5 1.0])
 x = rand(d)
-f(x) = logpdf(d, x)
+xv = Bijectors.VectorBijectors.to_linked_vec(d)(x)
+const invl = Bijectors.VectorBijectors.from_linked_vec(d)
+f(x) = logpdf(d, invl(x))
 
-@info f(x)
-@info gradient(Reverse, Const(f), x)
+@info f(xv)
+@info gradient(Reverse, Const(f), xv)
