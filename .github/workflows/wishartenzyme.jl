21d73752dda8a4abed0241426ee192b4e3d68d3e
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 20:53:58 +0100

    no dynamicppl?

--- .github/workflows/wishartenzyme.jl
+++ .github/workflows/wishartenzyme.jl
@@ -1,4 +1,13 @@
-using DynamicPPL, Distributions, LogDensityProblems, ADTypes, Enzyme
-@model f() = x ~ Wishart(7, [1.0 0.5; 0.5 1.0])
-adtype = AutoEnzyme(; mode=set_runtime_activity(Reverse), function_annotation=Const)
-DynamicPPL.TestUtils.AD.run_ad(f(), adtype; test=false, benchmark=true)
+# using DynamicPPL, Distributions, LogDensityProblems, ADTypes, Enzyme
+# @model f() = x ~ Wishart(7, [1.0 0.5; 0.5 1.0])
+# adtype = AutoEnzyme(; mode=set_runtime_activity(Reverse), function_annotation=Const)
+# DynamicPPL.TestUtils.AD.run_ad(f(), adtype; test=false, benchmark=true)
+
+using Distributions, Enzyme
+
+const d = Wishart(7, [1.0 0.5; 0.5 1.0])
+x = rand(d)
+f(x) = logpdf(d, x)
+
+@info f(x)
+@info gradient(Reverse, Const(f), x)
